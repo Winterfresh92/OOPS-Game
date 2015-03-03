a5dd0acc5feebd4ccf3f826d0c4cade2586cb4b6
Zain <Faaez@Faaez-PC>
2015-03-03 02:15:08 -0600

    Implemented the health and placed an stationary enemy to show collision and heath damage for player and updating the sprite for it. Right now it is only implemented for the demo purposes and will be fully implemented once the enemy class is fully implemented. The player's health decreases to half on collision with the enemy and happens only once in order to keep the player alive and  demonstrate damage on collision.

---
+++ OOP_Game/src/Enemy.java
@@ -0,0 +1,42 @@
+
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.Rectangle;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Faaez
+ */
+public class Enemy extends CollidableObject{
+    
+    Enemy(String ref, float x, float y)
+    {
+        super(ref,x,y);
+        this.solid = true;
+    }
+    
+     public Enemy(String ref, float x, float y, boolean solid, boolean mobile) {
+        super(ref, x, y);
+        this.width = 64;
+        this.height = 64;
+        this.solid = solid;
+        this.mobile = mobile;
+    }
+     
+    public boolean hitBy(Player p)
+    {
+        Rectangle bounds = new Rectangle((int)x - 64/2, (int)y - 64/2, 64, 64);
+        if(bounds.contains(new Point((int)p.getX(),(int)p.getY())))
+        {
+            return true;
+        }
+        else
+            return false;
+    }    
+     
+}
